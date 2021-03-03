c0ccdab2fa71d2599833f13fe4560238b6e0379d
muhammad hafeez <muhammadhafeez1719@yahoo.com>
2021-03-03 13:11:18 +0500

    react-covid-tracker-2019

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAIotMYnBfrTzswuW9kZJNDhN8X_VGz_Wg",
+    authDomain: "covid-19-trackers-2019.firebaseapp.com",
+    projectId: "covid-19-trackers-2019",
+    storageBucket: "covid-19-trackers-2019.appspot.com",
+    messagingSenderId: "252318823648",
+    appId: "1:252318823648:web:20baa88bec36b5bc398c85",
+    measurementId: "G-BH9DBC1ZN6"
+  };
