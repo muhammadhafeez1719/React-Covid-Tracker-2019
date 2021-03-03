96d7c60bb7140e0838daef987307174381785733
muhammad hafeez <muhammadhafeez1719@yahoo.com>
2021-03-03 13:31:29 +0500

    Updates

---
+++ precache-manifest.efb61831d8c494fffb724300f3071088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64f06eda0034315aea8de0d8cddc8a8",
+    "url": "/react-covid-tracker-2019/index.html"
+  },
+  {
+    "revision": "2fe6787c6c64da7d227f",
+    "url": "/react-covid-tracker-2019/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "40283b1323702af9bc8b",
+    "url": "/react-covid-tracker-2019/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "2fe6787c6c64da7d227f",
+    "url": "/react-covid-tracker-2019/static/js/2.e806ab36.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-covid-tracker-2019/static/js/2.e806ab36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40283b1323702af9bc8b",
+    "url": "/react-covid-tracker-2019/static/js/main.c551bda1.chunk.js"
+  },
+  {
+    "revision": "1ab43a800636da115d2e",
+    "url": "/react-covid-tracker-2019/static/js/runtime-main.27ce8da1.js"
+  }
+]);
